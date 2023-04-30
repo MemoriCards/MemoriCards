34595aa9e1fd198c26022519dd0dbb44ecb90ba1
ericvini000 <ericviniciusfelixsilva@gmail.com>
2023-04-30 19:06:22 -0400

    feat: RegisterForm and Schema

--- src/components/Form/LoginForm/LoginFormSchema.ts
+++ src/components/Form/LoginForm/LoginFormSchema.ts
@@ -1,8 +1,15 @@
 import { z } from "zod";
 
 export const LoginFormSchema = z.object({
-    email: z.string().nonempty("Campo Obrigatório").min(3,"Preencha o email corretamente").email("Preencha o email corretamente"),
-    password: z.string().nonempty().min(7, "A senha precisa ter no mínimo 7 caracteres")
-})
+  email: z
+    .string()
+    .nonempty("Campo Obrigatório")
+    .min(3, "Preencha o email corretamente")
+    .email("Preencha o email corretamente"),
+  password: z
+    .string()
+    .nonempty()
+    .min(7, "A senha precisa ter no mínimo 7 caracteres"),
+});
 
-export type TLoginValues = z.infer<typeof LoginFormSchema>
+export type TLoginValues = z.infer<typeof LoginFormSchema>;
