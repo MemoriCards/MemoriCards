c9417e38dfeb45b08c928cffb734537fde4944e1
Thomaz Rosseti <tocorosseti@gmail.com>
2023-05-01 11:21:33 -0300

    feat: errorPage_Done

---
+++ src/components/ErrorSection/index.tsx
@@ -0,0 +1,7 @@
+import error404 from "../../assets/Error404.svg"
+
+export const ErrorSection = () => (
+        <section>
+            <img src={error404} alt="Imagem de erro 404 (Page not found)" />
+        </section>
+);
---
+++ src/pages/ErrorPage/index.tsx
@@ -0,0 +1,11 @@
+import { ErrorSection } from "../../components/ErrorSection"
+
+const ErrorPage = () => {
+    return (
+        <>
+        <main>
+            <ErrorSection />
+        </main>
+        </>
+    )
+}
