f66d047c9607a1452b48c58446986743d4af8c1a
ericvini000 <ericviniciusfelixsilva@gmail.com>
2023-05-01 19:55:45 -0400

    feat: DashboardPage

--- src/components/Form/LoginForm/index.tsx
+++ src/components/Form/LoginForm/index.tsx
@@ -1,29 +1,32 @@
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { Input } from '../../../fragments/Input/index';
-import { LoginFormSchema, TLoginValues } from './LoginFormSchema';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useContext } from 'react';
-import { userContext } from '../../../providers/userContext';
-
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Input } from "../../../fragments/Input/index";
+import { LoginFormSchema, TLoginValues } from "./LoginFormSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from "react";
+import { userContext } from "../../../providers/userContext";
 
 export const LoginForm = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm<TLoginValues>({
-        resolver: zodResolver(LoginFormSchema)
-    });
-    const { doLogin } = useContext(userContext)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TLoginValues>({
+    resolver: zodResolver(LoginFormSchema),
+  });
+  const { doLogin } = useContext(userContext);
 
-    const submit: SubmitHandler<TLoginValues> = (formData) => doLogin(formData);
+  const submit: SubmitHandler<TLoginValues> = (formData) => doLogin(formData);
 
-    return (
-        <>
-            <h1>Entrar</h1>
-            <form onSubmit={handleSubmit(submit)} noValidate>
-                <Input label='Email' type='email' {...register("email")} />
-                {errors.email ? <p>{errors.email.message}</p> : null}
-                <Input label='Senha' type='password' {...register("password")} />
-                {errors.password ? <p>{errors.password.message}</p> : null}
-                <button type='submit'>Entrar</button>
-            </form>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Entrar</h1>
+      <form onSubmit={handleSubmit(submit)} noValidate>
+        <Input label="Email" type="email" {...register("email")} />
+        {errors.email ? <p>{errors.email.message}</p> : null}
+        <Input label="Senha" type="password" {...register("password")} />
+        {errors.password ? <p>{errors.password.message}</p> : null}
+        <button type="submit">Entrar</button>
+      </form>
+    </>
+  );
+};
