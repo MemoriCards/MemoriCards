9765b3cd36ee0fddf319aa6f18de31bfbe53ac05
Luana Carneiro <luanamaria2742@gmail.com>
2023-05-01 23:08:07 -0300

    feat : create cardContext

---
+++ src/providers/cardContext.tsx
@@ -0,0 +1,24 @@
+import { AxiosError } from "axios";
+import { createContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { api } from "../services/api";
+import { iUserProviderProps } from "./userContext";
+
+interface iCardContext {
+  cards: iCard | null;
+}
+
+interface iCard {}
+
+export const cardContext = createContext({} as iCardContext);
+
+export const CardProvider = ({ children }: iUserProviderProps) => {
+  const [cards, setCards] = useState<iCard | null>(null);
+
+  const navigate = useNavigate();
+
+  return (
+    <cardContext.Provider value={{ cards }}>{children}</cardContext.Provider>
+  );
+};
