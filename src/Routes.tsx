9e5f8a2dbd407d753b664f2a929425dd114cf79a
Luana Carneiro <luanamaria2742@gmail.com>
2023-05-02 01:25:39 -0300

    fix: merge conflicts

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,9 +6,9 @@ import { DashboardPage } from "./pages/DashboardPage";
 import { ErrorPage } from "./pages/ErrorPage";
 import { IndexPage } from "./pages/IndexPage";
 import { LoginPage } from "./pages/LoginPage";
-import { RegisterPage } from "./pages/RegisterPage";
 import { ResultPage } from "./pages/ResultPage";
 import { TestPage } from "./pages/TestPage";
+import { RegisterPage } from "./pages/RegisterPage";
 
 export const RoutesMain = () => {
   return (
