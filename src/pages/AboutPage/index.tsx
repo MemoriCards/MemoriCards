3fd5005343306d0ecd73c3dbf8f2ec294d45ab2b
Luana Carneiro <luanamaria2742@gmail.com>
2023-05-02 12:46:40 -0300

    feat : add Header to aboutPage

--- src/pages/AboutPage/index.tsx
+++ src/pages/AboutPage/index.tsx
@@ -1,35 +1,39 @@
+import { Header } from "../../components/Header";
+
 export const AboutPage = () => {
   return (
-    <main>
-      <section>
-        <h1>MemoriCards</h1>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam,
-          nostrum inventore? Ipsa sed incidunt porro dolor sequi id? Sed,
-          quaerat. Tempora corrupti reprehenderit, est maiores ipsam veniam
-          quaerat libero molestias
-        </p>
-       <img src="#" alt="#" />
-      </section>
-      <section>
-        <h2>Devs</h2>
-        <div>
-          <img src="#" alt="#" />
-          <h4>Kenzinho</h4>
-          <span>Dev Full Stack Master Blaster Top</span>
-        </div>
-        <div>
+    <>
+      <Header />
+      <main>
+        <section>
+          <h1>MemoriCards</h1>
 
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut officia
-            quis veniam aperiam blanditiis corporis? Ut veniam quibusdam minus
-            totam illo, nam, officia fuga rerum, ipsam similique facilis ex?
-            Tenetur!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam,
+            nostrum inventore? Ipsa sed incidunt porro dolor sequi id? Sed,
+            quaerat. Tempora corrupti reprehenderit, est maiores ipsam veniam
+            quaerat libero molestias
           </p>
-          <a href="#">Linkedin</a>
-        </div>
-      </section>
-    </main>
+          <img src="#" alt="#" />
+        </section>
+        <section>
+          <h2>Devs</h2>
+          <div>
+            <img src="#" alt="#" />
+            <h4>Kenzinho</h4>
+            <span>Dev Full Stack Master Blaster Top</span>
+          </div>
+          <div>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut
+              officia quis veniam aperiam blanditiis corporis? Ut veniam
+              quibusdam minus totam illo, nam, officia fuga rerum, ipsam
+              similique facilis ex? Tenetur!
+            </p>
+            <a href="#">Linkedin</a>
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
