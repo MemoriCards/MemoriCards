8455a24dcb4d5ac28988e01cf901bf9ae11cbaa5
Luana Carneiro <luanamaria2742@gmail.com>
2023-05-02 01:04:29 -0300

    fix : correct import

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import { ProtectedRoutes } from "./components/ProtectedRoutes";
 import { PublicRoutes } from "./components/PublicRoutes";
 import { AboutPage } from "./pages/AboutPage";
