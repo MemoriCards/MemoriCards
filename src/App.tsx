e6ddd3d548e8d0fcfe5a107d7c2c03546a16c77e
Luana Carneiro <luanamaria2742@gmail.com>
2023-05-01 21:46:11 -0300

    feat : add toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { RoutesMain } from "./Routes";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
+      <Toaster position="top-center" reverseOrder={false} />
       <RoutesMain />
     </>
   );
