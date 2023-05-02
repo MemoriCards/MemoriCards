9d574a8ff8b7bff335c218b7736d9532ae4da0da
ericvini000 <ericviniciusfelixsilva@gmail.com>
2023-05-02 17:39:48 -0400

    feat: results page

---
+++ src/components/ResultsContainer/index.tsx
@@ -0,0 +1,16 @@
+export const ResultsContainer = () => {
+
+    return (
+        <div>
+            <div>
+                <p>Acertos</p>
+                <span>0</span>
+                <p>Não respondida</p>
+                <span>0</span>
+                <p>Erradas</p>
+                <span>0</span>
+            </div>
+            <button>Tentar Novamente</button>
+        </div>
+    )
+}
