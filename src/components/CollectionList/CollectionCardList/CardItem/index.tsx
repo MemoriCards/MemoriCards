c005497b0e40757fa6f2d12384a44b3dd83bab60
math-rosa <math.rrosa@gmail.com>
2023-05-03 19:16:56 -0300

    feat: cardItem

--- src/components/CollectionList/CollectionCardList/CardItem/index.tsx
+++ src/components/CollectionList/CollectionCardList/CardItem/index.tsx
@@ -1,21 +1,29 @@
-import { useContext } from "react"
-import { cardContext, iCard } from "../../../../providers/cardContext"
+import { useContext } from "react";
+import { cardContext, iCard } from "../../../../providers/cardContext";
 
+import { Styledli } from "./style";
 
 interface ICardProps {
-    card: iCard;
+  card: iCard;
 }
 
 export const CardItem = ({ card }: ICardProps) => {
-    const { deleteCard, editCard } = useContext(cardContext);
-    return(
-        <li>
-            <div>
-                <button onClick={(() => editCard)}><i className="fa-sharp fa-solid fa-trash"></i></button>
-                <button onClick={(() => deleteCard)}></button>
-            </div>
-            <h3>{card.question}</h3>
-            <button type="button"><i className="fa-solid fa-arrow-pointer"/></button>
-        </li>
-    )
-}
+  const { deleteCard, editCard } = useContext(cardContext);
+  return (
+    <Styledli>
+      <div>
+        <button onClick={() => editCard}>
+          <i className="fa-sharp fa-solid fa-trash"></i>
+        </button>
+
+        <button onClick={() => deleteCard}>
+          <i className="fa-sharp fa-solid fa-trash"></i>
+        </button>
+      </div>
+      <h3>{card.question}</h3>
+      {/* <button type="button">
+        <i className="fa-solid fa-arrow-pointer" />
+      </button> */}
+    </Styledli>
+  );
+};
