b7aecf270492699fdab445862fba7f4b971c4139
Luana Carneiro <luanamaria2742@gmail.com>
2023-05-03 18:27:21 -0300

    feat : add toast error for naming in blank

--- src/components/CollectionName/index.tsx
+++ src/components/CollectionName/index.tsx
@@ -1,29 +1,41 @@
-import { useContext, useState } from "react"
+import { useContext, useState } from "react";
+import { toast } from "react-hot-toast";
 import { cardContext } from "../../providers/cardContext";
 
 export const CollectionName = () => {
+  const { inputValue, setInputValue, collectionTitle, setCollectionTitle } =
+    useContext(cardContext);
 
-    const {inputValue, setInputValue, collectionTitle, setCollectionTitle} = useContext(cardContext)
-
-    return (
-        <>
-            {inputValue ?
-                <div>
-                    <span>{collectionTitle}</span>
-                    <button onClick={() => setInputValue(false)}>
-                        <i className="fa-solid fa-pen"></i>
-                    </button>
-                </div>
-                :
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    setCollectionTitle(collectionTitle);
-                    setInputValue(true)
-                }}>
-                    <input type="text" value={collectionTitle} onChange={(e) => setCollectionTitle(e.target.value)} placeholder="De um nome para sua coleção de cartões..." />
-                    <button type="submit">OK</button>
-                </form>
+  return (
+    <>
+      {inputValue ? (
+        <div>
+          <span>{collectionTitle}</span>
+          <button onClick={() => setInputValue(false)}>
+            <i className="fa-solid fa-pen"></i>
+          </button>
+        </div>
+      ) : (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (collectionTitle != "") {
+              setCollectionTitle(collectionTitle);
+              setInputValue(true);
+            } else {
+              toast.error("Defina um nome válido");
             }
-        </>
-    )
-}
+          }}
+        >
+          <input
+            type="text"
+            value={collectionTitle}
+            onChange={(e) => setCollectionTitle(e.target.value)}
+            placeholder="De um nome para sua coleção de cartões..."
+          />
+          <button type="submit">OK</button>
+        </form>
+      )}
+    </>
+  );
+};
