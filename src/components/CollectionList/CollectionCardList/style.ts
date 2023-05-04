6db1b9858c6165bfe985d4877bc77cfc625cc2a7
math-rosa <math.rrosa@gmail.com>
2023-05-04 16:49:11 -0300

    fix: styled mobile

--- src/components/CollectionList/CollectionCardList/style.ts
+++ src/components/CollectionList/CollectionCardList/style.ts
@@ -3,11 +3,22 @@ import styled from "styled-components";
 export const StyledUl = styled.ul`
   display: flex;
   /* margin: auto; */
-  background: #FBFBFB;
+  background: #fbfbfb;
   overflow-x: auto;
   height: 100%;
   padding: 50px 100px;
   gap: 15px;
   flex-direction: row;
   flex-wrap: wrap;
+
+  @media (max-width: 900px) {
+    height: 450px;
+    gap: 25px;
+    padding: 50px 40px;
+    flex-direction: column;
+
+    /* overflow-y: scroll;
+    overflow-y: hidden;
+    white-space: nowrap; */
+  }
 `;
