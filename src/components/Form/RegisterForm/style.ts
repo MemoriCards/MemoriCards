5796a0a5331eae9b569a2f29099dce962d1e8d3e
niljoshua <niljoshuaa@gmail.com>
2023-05-04 12:38:39 -0300

    mobile-login/register

--- src/components/Form/RegisterForm/style.ts
+++ src/components/Form/RegisterForm/style.ts
@@ -3,6 +3,39 @@ import  styled  from 'styled-components';
 
 
 export const RegisterDivStyle = styled.div `
+
+        display: flex;
+        margin: 0px auto;
+        flex-direction: column;
+        align-items: center;
+        width: 90%;
+        height: 400px;
+        background-color: var(--grey-0);
+        border-radius: 8px;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        justify-content: space-evenly;
+
+        form {
+            gap: 20px;
+            display: flex;
+            flex-direction: column;
+            }
+
+            button {
+                height: 42px;
+                width: 250px;
+                left: 536px;
+                top: 608px;
+                border-radius: 7px;
+                background-color: var(--color-secundary-3);
+                color: var(--grey-0);
+                font-size: 16px;
+                margin-top: 50px;
+            }
+}       
+
+
+    @media (min-width: 600px) {
         display: flex;
         margin: 0px auto;
         flex-direction: column;
@@ -14,21 +47,23 @@ export const RegisterDivStyle = styled.div `
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
         justify-content: space-evenly;
 
-        form {
+            form {
             gap: 30px;
             display: flex;
             flex-direction: column;
-        }
+            }
 
-        button {
-            height: 42px;
-            width: 368px;
-            left: 536px;
-            top: 608px;
-            border-radius: 7px;
-            background-color: var(--color-secundary-3);
-            color: var(--grey-0);
-            font-size: 16px;
-            margin-top: 50px;
+            button {
+                height: 42px;
+                width: 368px;
+                left: 536px;
+                top: 608px;
+                border-radius: 7px;
+                background-color: var(--color-secundary-3);
+                color: var(--grey-0);
+                font-size: 16px;
+                margin-top: 50px;
+            }
         }
+    }
 `
--- src/pages/AboutPage/style.ts
+++ src/pages/AboutPage/style.ts
@@ -1,94 +1,185 @@
 import styled from "styled-components";
 
 export const StyledMain = styled.main`
-  display: flex;
-  
-  flex-direction: column;
-  align-items: center;
-  height: 90vh;
-
-  section {
-    margin-top: 30px;
-    width: 990px;
-    height: 365px;
-    background: var(--color-primary-1);
-    color: white;
-    border: 1px solid rgba(0, 0, 0, 0.1);
-    border-radius: 7px;
-    margin-bottom: 10px;
 
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 30px;
+    height: 90vh;
 
-    
+    section {
+      margin-top: 30px;
+      width: 90%;
+      height: 280px;
+      background: var(--color-primary-1);
+      color: white;
+      border: 1px solid rgba(0, 0, 0, 0.1);
+      border-radius: 7px;
+      margin-bottom: 10px;
 
-
-    h2 {
-      font-weight: 700;
-      font-size: 28px;
-
-    }
-
-    p {
-      margin-top: 50px;
-      font-weight: 500;
-      font-size: 26px;
-      padding: 0 100px;
-    }
-
-    :nth-child(2n) {
-      background: var(--grey-0);
-      color: black;
       display: flex;
       flex-direction: column;
       align-items: center;
-      align-content: center;
-      justify-content: center;
-      gap: 2rem;
+      padding: 30px;  
 
-      ul {
+      h2 {
+        font-weight: 500;
+        font-size: 28px;
+      }
+
+      p {
+        margin-top: 20px;
+        font-weight: 300;
+        font-size: 16px;
+        padding: 0 10px;
+        text-align: justify;
+      }
+
+      :nth-child(2n) {
+        background: var(--grey-0);
+        color: black;
         display: flex;
-        gap: 0.5rem;
+        flex-direction: column;
+        align-items: center;
+        align-content: center;
+        justify-content: center;
+        gap: 2rem;
+        height: 400px;
 
-        li {
-          background: #d9d9d9;
-          border: 1px solid #000000;
-          border-radius: 7px;
-          width: 180px;
-          height: 234px;
+        ul {
+          display: flex;
+          gap: 0.5rem;
 
+          li {
+            background: #d9d9d9;
+            border: 1px solid #000000;
+            border-radius: 7px;
+            width: 180px;
+            height: 234px;
+
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            align-content: center;
+            gap: 0.5rem;
+
+            padding: 25px 0;
+
+            h4 {
+              font-weight: 700;
+              font-size: 16px;
+            }
+
+            h5 {
+              font-weight: 500;
+              font-size: 15px;
+            }
+          }
+        }
+      }
+
+      li > img {
+        height: 93px;
+        border: 3px solid #1a59fc;
+        border-radius: 50%;
+      }
+
+      a {
+        height: 10px;
+        margin-right: 10px;
+      }
+    }
+
+    @media (min-width: 600px) {
+      display: flex;
+      
+      flex-direction: column;
+      align-items: center;
+      height: 90vh;
+
+      section {
+        margin-top: 30px;
+        width: 990px;
+        height: 365px;
+        background: var(--color-primary-1);
+        color: white;
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        border-radius: 7px;
+        margin-bottom: 10px;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 30px;
+
+        
+
+
+        h2 {
+          font-weight: 700;
+          font-size: 28px;
+
+        }
+
+        p {
+          margin-top: 50px;
+          font-weight: 500;
+          font-size: 26px;
+          padding: 0 100px;
+        }
+
+        :nth-child(2n) {
+          background: var(--grey-0);
+          color: black;
           display: flex;
           flex-direction: column;
           align-items: center;
           align-content: center;
-          gap: 0.5rem;
+          justify-content: center;
+          gap: 2rem;
 
-          padding: 25px 0;
+          ul {
+            display: flex;
+            gap: 0.5rem;
 
-          h4 {
-            font-weight: 700;
-            font-size: 16px;
+            li {
+              background: #d9d9d9;
+              border: 1px solid #000000;
+              border-radius: 7px;
+              width: 180px;
+              height: 234px;
+
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              align-content: center;
+              gap: 0.5rem;
+
+              padding: 25px 0;
+
+              h4 {
+                font-weight: 700;
+                font-size: 16px;
+              }
+
+              h5 {
+                font-weight: 500;
+                font-size: 15px;
+              }
+            }
           }
+        }
 
-          h5 {
-            font-weight: 500;
-            font-size: 15px;
-          }
+        li > img {
+          height: 93px;
+          border: 3px solid #1a59fc;
+          border-radius: 50%;
+        }
+
+        a {
+          height: 10px;
+          margin-right: 10px;
         }
       }
     }
-
-    li > img {
-      height: 93px;
-      border: 3px solid #1a59fc;
-      border-radius: 50%;
-    }
-
-    a {
-      height: 10px;
-      margin-right: 10px;
-    }
-  }
 `;
